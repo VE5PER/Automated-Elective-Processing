3222789bf2cd4fbd7f47056c255c92e6b422ca3b
SAURAV <sauravshyju@gmail.com>
2022-03-26 12:26:49 +0530

    added quotes slider

--- CODE/FRONTEND/lib/pages/dashboard/widget/recruitment_data_widget.dart
+++ CODE/FRONTEND/lib/pages/dashboard/widget/recruitment_data_widget.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:automated_elective_processing/common/app_colors.dart';
 
@@ -11,155 +12,49 @@ class _RecruitmentDataWidgetState extends State<RecruitmentDataWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-          color: AppColor.white, borderRadius: BorderRadius.circular(20)),
-      padding: EdgeInsets.all(20),
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                "Electives",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: AppColor.black,
-                  fontSize: 22,
-                ),
-              ),
-            ],
-          ),
-          Divider(
-            thickness: 0.5,
-            color: Colors.grey,
-          ),
-          Table(
-            defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-            children: [
-              /// Table Header
-              TableRow(
-                decoration: BoxDecoration(
-                  border: Border(
-                      bottom: BorderSide(
-                    color: Colors.grey,
-                    width: 0.5,
-                  )),
-                ),
-                children: [
-                  tableHeader("Full Name"),
-                    tableHeader("Designation"),
-                  tableHeader("Status"),
-                ],
-              ),
+child: CarouselSlider(
+    options: CarouselOptions(
+      height: 400,
+      aspectRatio: 16/9,
+      viewportFraction: 0.8,
+      initialPage: 0,
+      enableInfiniteScroll: true,
+      reverse: false,
+      autoPlay: true,
+      autoPlayInterval: Duration(seconds: 3),
+      autoPlayAnimationDuration: Duration(milliseconds: 800),
+      autoPlayCurve: Curves.fastOutSlowIn,
+      enlargeCenterPage: true,
+      scrollDirection: Axis.horizontal,
+    ),
+  items: ["When you have a dream, you’ve got to grab it and never let go. — Carol Burnett",
 
-              /// Table Data
-              tableRow(
-                context,
-                name: "Mary G Lolus",
-                color: Colors.blue,
-                image: "assets/user1.jpg",
-                designation: "Project Manager",
-                status: "Practical Round",
-              ),
-              tableRow(
-                context,
-                name: "Vince Jacob",
-                color: Colors.blue,
-                image: "assets/user2.jpg",
-                designation: "UI/UX Designer",
-                status: "Practical Round",
-              ),
-              tableRow(
-                context,
-                name: "Nell Brian",
-                color: Colors.green,
-                image: "assets/user3.jpg",
-                designation: "React Developer",
-                status: "Final Round",
-              ),
-              tableRow(
-                context,
-                name: "Vince Jacob",
-                color: Colors.yellow,
-                image: "assets/user2.jpg",
-                designation: "UI/UX Designer",
-                status: "HR Round",
-              ),
-            ],
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("Showing 4 out of 4 Results"),
-                Text(
-                  "View All",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-              ],
+"Nothing is impossible. The word itself says ‘I’m possible!' — Audrey Hepburn",
+
+    "There is nothing impossible to they who will try. — Alexander the Great",
+
+"The bad news is time flies. The good news is you’re the pilot. — Michael Altshuler",
+
+    "Life has got all those twists and turns. You’ve got to hold on tight and off you go. — Nicole Kidman"].map((i) {
+    return Builder(
+      builder: (BuildContext context) {
+        return Container(
+          padding: EdgeInsets.all(20),
+            width: MediaQuery.of(context).size.width,
+            margin: EdgeInsets.symmetric(horizontal: 5.0),
+            decoration: BoxDecoration(
+                color: Colors.blue[100],
+              borderRadius:  BorderRadius.all(Radius.circular(20))
             ),
-          )
-        ],
-      ),
+
+            child: Center(child: Text('-> $i <-', style: TextStyle(fontSize:40.0, fontStyle: FontStyle.italic),))
+        );
+      },
     );
-  }
+  }).toList(),
 
-  TableRow tableRow(context, {name, image, designation, status, color}) {
-    return TableRow(
-        decoration: BoxDecoration(
-          border: Border(
-            bottom: BorderSide(
-              color: Colors.grey,
-              width: 0.5,
-            ),
-          ),
-        ),
-        children: [
-          //Full Name
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 15),
-            child: Row(
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(1000),
-                  child: Image.asset(
-                    image,
-                    width: 30,
-                  ),
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                Text(name)
-              ],
-            ),
-          ),
-          // Designation
-          //Status
-          Row(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: color,
-                ),
-                height: 10,
-                width: 10,
-              ),
-              SizedBox(
-                width: 10,
-              ),
-              Text(status),
-            ],
-          ),
-          // Menu icon
-            Image.asset(
-              "assets/more.png",
-              color: Colors.grey,
-              height: 30,
-            )
-        ]);
+),
+        );
   }
 
   Widget tableHeader(text) {
