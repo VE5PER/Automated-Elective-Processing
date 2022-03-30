05d80c51e91500b968eff1129d1205070277b770
Viswabharathi-K-A <viswabharathika@gmail.com>
2022-03-30 10:27:17 +0530

    added back button to WIP

--- CODE/caut_front/lib/pages/WIP.dart
+++ CODE/caut_front/lib/pages/WIP.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:automated_elective_processing/dashboard.dart';
+import 'package:automated_elective_processing/common/app_colors.dart';
+
 
 class WIP extends StatelessWidget {
   const WIP({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Icon(Icons.construction, size: 500,),);
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(icon: Icon(Icons.arrow_back,color: AppColor.yellow), onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context) => dashboard()));} ),
+      ),
+      body: Icon(Icons.construction, size: 500,),);
   }
 }
